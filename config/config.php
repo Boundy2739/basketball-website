c18dec8364eea242194359a4530edd4b582f4e0c
Boundy2739 <boundy42@gmail.com>
2026-04-18 19:10:59 +0100

    Add timer based session

--- config/config.php
+++ config/config.php
@@ -7,3 +7,40 @@ if ($_SERVER['HTTP_HOST'] === 'localhost' || $_SERVER['HTTP_HOST'] === '127.0.0.
     // InfinityFree 
     define('BASE_URL', 'https://hoop-avenue.infinityfree.me/');
 }
+
+session_start();
+
+if (!isset($_SESSION['last_regeneration'])) {
+    session_regenerate_id(true);
+    $_SESSION['last_regeneration'] = time();
+} else {
+    $timer = 60 * 30;
+    if (time() - $_SESSION['last_regeneration'] >= $timer) {
+        session_regenerate_id(true);
+        $_SESSION['last_regeneration'] = time();
+    };
+}
+
+/*Destroys session if someone tries to access from different device or with a different ip address */
+if (isset($_SESSION['ip'], $_SESSION['user_agent'])) {
+    if (
+        $_SESSION['ip'] !== $_SERVER['REMOTE_ADDR'] ||
+        $_SESSION['user_agent'] !== $_SERVER['HTTP_USER_AGENT']
+    ) {
+
+        session_unset();
+        session_destroy();
+        header('Location: ../index.php');
+        exit;
+    }
+}
+$idleTimer = 90;
+if (isset($_SESSION['last_activity']) && (time() - $_SESSION['last_activity']) > $idleTimer) {
+    session_unset();
+    session_destroy();
+    header('Location: ../index.php');
+    exit;
+}
+
+
+
